a8f3f143f93f47caf61b6215de758fe37a0cf45f
FranciscoBarCa <60709314+FranciscoBarCa@users.noreply.github.com>
2022-08-29 22:04:21 +0200

    Finished first version of code generation

--- src/components/ExportCode/main.ts
+++ src/components/ExportCode/main.ts
@@ -9,22 +9,77 @@ import NodeGenerator from "./nodeGenerator";
 var zip = require("jszip")();
 
 const CodeGenerator = (nodes: NodeProps[], topics: TopicProps[], devices: DeviceProps[]) => {
-  var code = "Hello World";
-  nodes.forEach((node) => {
-    code = NodeGenerator(node);
-    node.publishers.forEach((publisher) => {
-      code += TopicGenerator(publisher);
-    }),
-      node.subscribers.forEach((subscriber) => {
-        code += TopicGenerator(subscriber);
-      }),
+
+  
+  devices.forEach(device => {
+  var nodesinPackage = nodes.filter(node => node.device === device.id);
+  nodesinPackage.forEach((node) => {
+  var code = `#!usr/bin/env/python3\n# -*- coding: utf-8 -*-\nimport rclpy\nfrom rclpy.node import Node\n`;
+  // array of topics found to be subscribed to
+  var subscribers : any[] = [];
+  node.subscribers.forEach((subscriber) => {
+    subscribers.push({... topics.find((topic) => topic.id === subscriber.topicID), rate: subscriber.rate});
+  });
+  // array of topics found to be published to
+  var publishers: any[] = [];
+  node.publishers.forEach((publisher) => {
+    publishers.push({...topics.find((topic) => topic.id === publisher.topicID),rate: publisher.rate});
+  });
+  // array of types of topics found to be subscribed and published to
+  let uniquetypes = [
+    ...new Map([...subscribers, ...publishers].map((item) => [item?.type.type, item?.type])).values(),
+  ];
+  uniquetypes.forEach((type) => {
+    code += `\nfrom ${type?.class}.msg import ${type?.type.split(".")[0]}`;
+  });
+
+  code += `\n\nclass ${node.label}(Node):
+    def __init__(self):
+      super().__init__('${node.label}')
+      self.get_logger().info('${node.label} node started')`;
+
+  subscribers.forEach((subscriber) => {
+    code += `\n        self.${subscriber?.label}_subscriber = self.create_subscription(${subscriber?.type.type.split(".")[0]}, '${subscriber?.label}', self.${subscriber?.label}_callback, 10)`;
+  });
+
+  publishers.forEach((publisher) => {
+    code += `\n        self.${publisher?.label}_publisher = self.create_publisher(${publisher?.type.type.split(".")[0]}, '${publisher?.label}',10)\n        self.create_timer(0.5, self.send_cmd_vel)`; 
+    
+  });
+
+  subscribers.forEach((subscriber) => {
+    code += `\n    def ${subscriber?.label}_callback(self, msg):
+      self.get_logger().info('I received a ${subscriber?.label} message')`;
+  }
+  );
+  publishers.forEach((publisher) => {
+    code += `\n    def send_${publisher.label}(self):
+      msg = ${publisher?.type.type.split(".")[0]}()
+      ###########################
+      #add message content here
+      ###########################
+      self.${publisher?.label}_publisher.publish(msg)
+      self.get_logger().info('I published a ${publisher?.label} message')`;
+  }
+  );
+    code += `\ndef main(args=None):
+    rclpy.init(args=args) #Initialize rclpy (ROS2)
+    node = DrawCircleNode()    #Create a node of class DrawCircleNode
+    rclpy.spin(node)      #Spin the node until the program is interrupted Keep the node alive until Ctrl+C is pressed
+    rclpy.shutdown()    #Destroy the node before closing the program
+
+def __init__(self): 
+    main() 
+    `
       zip.file(node.label + ".py", code);
   });
-  // zip.file("Hello.txt", "Hello World\n");
-  console.log(nodes);
+  
+  
   zip.generateAsync({ type: "blob" }).then(function (content: any) {
     // see FileSaver.js
-    saveAs(content, "example.zip");
+    saveAs(content, `Package_${device.name}.zip`);
   });
+});
+
 };
 export default CodeGenerator;
